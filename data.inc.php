3fc8390f80fe2faf364862555a6cb3e623df3d9e
Tobias Bäthge <git@tobias.baethge.com>
2012-12-16 13:16:01 +0100

    Add TablePress specific modifications, based on Jetpack modifications

--- data.inc.php
+++ data.inc.php
@@ -655,3 +655,6 @@ $GLOBALS['csstidy']['predefined_templates']['low_compression'][] = '	';
 $GLOBALS['csstidy']['predefined_templates']['low_compression'][] = '<span class="comment">'; // before comment
 $GLOBALS['csstidy']['predefined_templates']['low_compression'][] = '</span>'."\n"; // after comment
 $GLOBALS['csstidy']['predefined_templates']['low_compression'][] = "\n";
+
+// Add TablePress specific modifications, based on Jetpack modifications
+require dirname( __FILE__ ) . '/data-tp.inc.php';
